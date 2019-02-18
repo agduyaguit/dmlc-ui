2ac47f392ad32a1961bcb96fc1d44eb0b71a907a
Arnil Duyaguit <agduyaguit@Arnils-MacBook-Air.local>
2019-02-19 00:00:55 +0900

    Add navigation component

--- app/components/Navigation/tests/index.test.js
+++ app/components/Navigation/tests/index.test.js
@@ -2,9 +2,9 @@
 // import { mount } from 'enzyme';
 // import { enzymeFind } from 'styled-components/test-utils';
 
-// import Link from '../index';
+// import Navigation from '../index';
 
-describe('<Link />', () => {
+describe('<Navigation />', () => {
   it('Expect to have unit tests specified', () => {
     expect(true).toEqual(false);
   });
